bcccce6f2b012d9f2dcfe70163fea79f539d8b01
EnderHisham <hishamsohail02@gmail.com>
2025-05-17 20:30:47 +0500

    Add files via upload

---
+++ lsss web page/script.js
@@ -0,0 +1,7 @@
+//alert("hi there person welcome to lss web page");
+console.log ("hay there welcome to the official web page of little star school")
+let a = 'hay there';
+console.log (a)
+let b = '1311'
+let c = '2138'
+console.log (b + c)
